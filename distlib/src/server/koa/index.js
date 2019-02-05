5681f2a2a98dcacfc65b20877a999cf8af9a94f7
cbhubbard <hubbard8@gmail.com>
2019-02-05 11:59:10 -0500

    Fixing directory structure

--- distlib/src/server/koa/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//Module Dependencies
-const Server_1 = require("./Server");
-//Set the app
-let app = new Server_1.default();
-//# sourceMappingURL=index.js.map
