586743478cfb5d2a39c99dbaa6bc487281976d5a
cbhubbard <hubbard8@gmail.com>
2019-02-06 14:29:01 -0500

    Added unit testing structure -- project now ready for frontend dev

---
+++ src/client/tests/index.spec.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as TestUtils from 'react-dom/test-utils';
+
+it("Test check on the index", () => {
+
+    expect("Hello").toEqual("Hello")
+});
