5ca59c414cf55e8b5ad9cf6f9b1de4e170fd9d32
cbhubbard <hubbard8@gmail.com>
2019-04-05 10:27:16 -0400

    Finished Client Integration with AWS Cognito

--- src/client/config/index.ts
+++ src/client/config/index.ts
@@ -6,18 +6,28 @@ const allConfigs: any =
     LOCAL: 
     {
         apiUrl: 'http://localhost:8000',
-        moneydUrl: 'http://localhost:8081'
+        moneydUrl: 'http://localhost:8081',
+        cognito: {
+            REGION: 'us-east-2',
+            USER_POOL_ID: 'us-east-2_dXzoq9vL3',
+            APP_CLIENT_ID: '3ef6fitjfku707tu30esnuhqj7'
+        }
     },
     DEV:
     {
         apiUrl: 'https://70es1m90b3.execute-api.us-east-2.amazonaws.com/dev',
-        //apiUrl: 'https://8k3zb9t689.execute-api.us-east-2.amazonaws.com/dev',
-        moneydUrl: 'http://localhost:8081'
+        moneydUrl: 'http://localhost:8081',
+        cognito: {
+            REGION: 'us-east-2',
+            USER_POOL_ID: 'us-east-2_dXzoq9vL3',
+            APP_CLIENT_ID: '3ef6fitjfku707tu30esnuhqj7'
+        }
     },
     PROD:
     {
         apiUrl: '',
-        moneydUrl: ''
+        moneydUrl: '',
+        cognito: {}
     }
 };
 
--- src/client/models/user.model.ts
+++ src/client/models/user.model.ts
@@ -3,5 +3,6 @@ export interface User
     email: string,
     password?: string,
     firstname?: string,
-    lastname?: string
+    lastname?: string,
+    email_verified?: boolean
 }
